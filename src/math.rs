cc5b233789e1a85bd5bab1f0ca1bc9c9209b5658
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-21 16:06:43 -0400

    feat: created math functions

---
+++ src/math.rs
@@ -0,0 +1,15 @@
+fn add(x: i32, y: i32) -> i32 {
+    return x + y;
+}
+
+fn subtract(x: i32, y: i32) -> i32 {
+    return x - y;
+}
+
+fn multiply(x: i32, y: i32) -> i32 {
+    return x * y;
+}
+
+fn divide(x: i32, y: i32) -> i32 {
+    return x / y;
+}
