ad0f5b3d7322af3ef6d90732a4c0e35e3a054554
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-21 16:26:12 -0400

    refactor: example usage of function

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use mathemizer::{
+    add,
+    divide,
+    multiply,
+    subtract,
+};
+
+fn main() {
+    println!("Hello, world!");
+    println!("{}", add(1, 2));
+    println!("{}", subtract(1, 2));
+    println!("{}", multiply(1, 2));
+    println!("{}", divide(1, 2));
+}
